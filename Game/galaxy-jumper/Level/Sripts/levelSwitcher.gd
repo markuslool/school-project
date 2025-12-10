c84d279d835ea47545d67fd7be7445214202f1ec
Retren45 <antontyalin45@gmail.com>
2025-12-10 23:35:12 +0200

    beta_0.4.4
    menu problem

--- Game/galaxy-jumper/Level/Sripts/levelSwitcher.gd
+++ Game/galaxy-jumper/Level/Sripts/levelSwitcher.gd
@@ -1,11 +1,13 @@
 extends Node
 
+# Пути к уровням
 var level_paths = {
 	"Level_01": "res://Level/Scenes/Level_01.tscn",
 	"Level_02": "res://Level/Scenes/Level_02.tscn",
 	"Level_03": "res://Level/Scenes/Level_03.tscn"
 }
 
+# Сюда будем хранить текущую сцену (меню или уровень)
 var current_scene: Node = null
 
 func _ready():
@@ -20,16 +22,17 @@ func _ready():
 				load_level(button.name.replace("TestButton", "Level_0"))
 			)
 
-
 # Функция загрузки уровня
 func load_level(level_name: String):
-	# Удаляем старую сцену (меню или предыдущий уровень)
-	if current_scene:
-		current_scene.queue_free()
-		current_scene = null
-
 	# Загружаем новый уровень
 	var level_scene = load(level_paths.get(level_name, ""))
 	if level_scene:
-		current_scene = level_scene.instantiate()
-		add_child(current_scene)
+		var new_level = level_scene.instantiate()
+		add_child(new_level)
+
+		# После успешной загрузки удаляем старую сцену
+		if current_scene:
+			current_scene.queue_free()
+		
+		# Обновляем текущую сцену
+		current_scene = new_level
