54f7e56bb7edc6e4972bab2d510126bd0f29a4b7
Retren45 <antontyalin45@gmail.com>
2025-12-11 00:25:09 +0200

    level_01
    new level

--- Game/galaxy-jumper/Level/Sripts/platformMover.gd
+++ Game/galaxy-jumper/Level/Sripts/platformMover.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+# Точки движения
 var point_a := Vector2()
 var point_b := Vector2()
 var speed := 100
@@ -8,9 +9,15 @@ var target_pos := Vector2()
 var wait_time := 1.0      # задержка в секундах
 var wait_left := 0.0      # внутренний таймер ожидания
 
+var hight := 100          # значение по умолчанию, если метаданных нет
+
 func _ready():
+	# Получаем метаданные "Hight", если они есть
+	if has_meta("Hight"):
+		hight = int(get_meta("Hight"))
+	
 	point_a = global_position
-	point_b = Vector2(point_a.x, point_a.y + 110)  # смещение по Y
+	point_b = Vector2(point_a.x, point_a.y + hight)
 	target_pos = point_b
 
 func _physics_process(delta):
@@ -25,4 +32,5 @@ func _physics_process(delta):
 	# Проверяем достижение цели
 	if global_position.distance_to(target_pos) < 1:
 		wait_left = wait_time                        # запустить задержку
+		# Меняем цель
 		target_pos = point_a if target_pos == point_b else point_b
