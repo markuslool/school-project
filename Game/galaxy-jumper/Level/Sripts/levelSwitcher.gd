4a3ff126c2d16fa20c26415a60d35d75efb48972
Retren45 <antontyalin45@gmail.com>
2025-12-10 23:17:05 +0200

    beta_0.4.3
    main menu

---
+++ Game/galaxy-jumper/Level/Sripts/levelSwitcher.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var level_paths = {
+	"Level_01": "res://Level/Scenes/Level_01.tscn",
+	"Level_02": "res://Level/Scenes/Level_02.tscn",
+	"Level_03": "res://Level/Scenes/Level_03.tscn"
+}
+
+var current_scene: Node = null
+
+func _ready():
+	# Получаем меню напрямую из корня сцены
+	current_scene = get_node("/root/Main_Menu") # путь указываем согласно твоей иерархии
+
+	# Подключаем сигналы кнопок меню
+	var buttons_node = current_scene.get_node("Buttons")
+	for button in buttons_node.get_children():
+		if button is Button:
+			button.pressed.connect(func():
+				load_level(button.name.replace("TestButton", "Level_0"))
+			)
+
+
+# Функция загрузки уровня
+func load_level(level_name: String):
+	# Удаляем старую сцену (меню или предыдущий уровень)
+	if current_scene:
+		current_scene.queue_free()
+		current_scene = null
+
+	# Загружаем новый уровень
+	var level_scene = load(level_paths.get(level_name, ""))
+	if level_scene:
+		current_scene = level_scene.instantiate()
+		add_child(current_scene)
