d4edc6450e7721d4aadb6cb00ef4622c6192987e
Mark Khrystoforov <hristofor@school91.org.ua>
2025-12-11 20:18:44 +0200

    win menu

--- Game/galaxy-jumper/Level/Sripts/GameWinMenu.gd
+++ Game/galaxy-jumper/Level/Sripts/GameWinMenu.gd
@@ -1,15 +1,38 @@
 extends Control
 
+var current_level: String = ""
+
 func _ready():
 	visible = false
 
-func show_game_over():
+func show_game_win():
 	visible = true
 	get_tree().paused = true
+	# Определяем текущий уровень из имени сцены
+	var scene_path = get_tree().current_scene.scene_file_path
+	if "Level_01" in scene_path:
+		current_level = "Level_02"
+	elif "Level_02" in scene_path:
+		current_level = "Level_03"
+	else:
+		current_level = ""
 
-func _on_restart_button_pressed():
+func _on_next_level_button_pressed():
 	get_tree().paused = false
-	get_tree().reload_current_scene()
+	if current_level != "":
+		var level_paths = {
+			"Level_02": "res://Level/Scenes/Level_02.tscn",
+			"Level_03": "res://Level/Scenes/Level_03.tscn"
+		}
+		var next_level_path = level_paths.get(current_level, "")
+		if next_level_path != "":
+			get_tree().change_scene_to_file(next_level_path)
+		else:
+			# Если следующего уровня нет, возвращаемся в главное меню
+			get_tree().change_scene_to_file("res://Level/Scenes/Main_Menu.tscn")
+	else:
+		# Если уровень не определен, возвращаемся в главное меню
+		get_tree().change_scene_to_file("res://Level/Scenes/Main_Menu.tscn")
 
 func _on_main_menu_button_pressed():
 	get_tree().paused = false
--- Game/galaxy-jumper/Level/Sripts/playerController.gd
+++ Game/galaxy-jumper/Level/Sripts/playerController.gd
@@ -5,13 +5,18 @@ var gravity = 900            # сила гравитации
 var jump_force = -400        # сила прыжка
 
 var game_over_menu_scene = preload("res://Level/Scenes/GameOverMenu.tscn")
+var game_win_menu_scene = preload("res://Level/Scenes/GameWinMenu.tscn")
 var game_over_menu
+var game_win_menu
+var level_completed: bool = false
 
 func _ready():
 	var canvas_layer = CanvasLayer.new()
 	add_child(canvas_layer)
 	game_over_menu = game_over_menu_scene.instantiate()
+	game_win_menu = game_win_menu_scene.instantiate()
 	canvas_layer.add_child(game_over_menu)
+	canvas_layer.add_child(game_win_menu)
 
 func _physics_process(delta):
 	# Применяем гравитацию
@@ -29,15 +34,23 @@ func _physics_process(delta):
 	# Двигаем тело
 	move_and_slide()
 	
-	# Проверка столкновения с шипами
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var collider = collision.get_collider()
-		if collider.name == "Spikes":
-			game_over()
-		if collider.name == "Saw":
-			game_over()
+	# Проверка столкновения с шипами и концом уровня
+	if not level_completed:
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var collider = collision.get_collider()
+			if collider.name == "Spikes":
+				game_over()
+			if collider.name == "Saw":
+				game_over()
+			if collider.name == "EndLevel":
+				level_complete()
 
 func game_over():
 	if game_over_menu:
 		game_over_menu.show_game_over()
+
+func level_complete():
+	if game_win_menu and not level_completed:
+		level_completed = true
+		game_win_menu.show_game_win()
