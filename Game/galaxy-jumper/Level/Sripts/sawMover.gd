37d572f323d1992cd4943ce5404c5f403f8616d0
Retren45 <antontyalin45@gmail.com>
2025-12-13 21:44:09 +0200

    Level_Update_0.2

--- Game/galaxy-jumper/Level/Sripts/sawMover.gd
+++ Game/galaxy-jumper/Level/Sripts/sawMover.gd
@@ -31,7 +31,7 @@ func _physics_process(delta: float) -> void:
 	if wait_left > 0.0:
 		wait_left = max(wait_left - delta, 0.0)
 		if anim:
-			anim.play("Idle")
+			anim.play("Work")
 		return
 
 	if anim:
